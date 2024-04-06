1a85764ae801ff3254cbada67dc0485fd2722c3f
Edward <50258228+or-yarok@users.noreply.github.com>
2024-04-06 21:56:35 +0500

    Update bot.py big update

--- bot.py
+++ bot.py
@@ -2,10 +2,18 @@ import telebot
 from dotenv import load_dotenv
 import os
 from dataclasses import dataclass
-from typing import Dict
+from typing import Dict, List, Optional
 import re
 from g4f.client import Client
+import csv
 
+contracts_csv_filename = "contracts_list.csv"
+
+with open(contracts_csv_filename, newline='', encoding='utf-8') as csvfile:
+    reader = csv.DictReader(csvfile, delimiter=',')
+    contracts =[]
+    for row in reader:
+        contracts.append(row)
 
 load_dotenv()
 
@@ -22,12 +30,13 @@ if not API_KEY:
         )
 MODEL = "gpt-3.5-turbo"
 
-LANG = "English"  # language by default
+LANG = "Russian"  # language by default
 
 LANGUAGES_LIST = (
     'English',
     'Russian',
     'Georgian',
+    'Chinese',
 )
 
 # language selection keyboard
@@ -39,6 +48,7 @@ language_buttons = [reply_btn(lang) for lang in LANGUAGES_LIST]
 language_kb = reply_kb.add(*language_buttons)
 
 bot = telebot.TeleBot(TOKEN, parse_mode='MarkdownV2')
+
 
 def escape(text: str) -> str:
     """
@@ -65,13 +75,34 @@ class User:
    chat_id: int
    name: str
    language: str = "English"
+   contract_number: Optional[str] = None
 
 users: Dict[User_id, User] = {}  # dictionary of users
+
+
+@dataclass
+class Question:
+    text: str
+    buttons: Optional[List[telebot.types.InlineKeyboardButton]]
+
+
+questions: Dict[str, Question] = {
+    "GettingKnow": Question(text="Вы уже являетесь клиентом XPAGE?",
+                            buttons=[btn('Да', callback_data="Q01_Y"),
+                                     btn('Нет', callback_data="Q01_N")]),
+    "ContractNum": Question(text="Пожалуйста, сообщите номер вашего договора с XPAGE",
+                            buttons=None),
+    "NewUser":     Question(text="Чем я могу вам помочь?",
+                            buttons=[btn('Направить шаблон договора', callback_data="Q02_contract"),
+                                     btn('Рассказать о компании XPAGE', callback_data="Q02_about"),
+                                     btn('Примеры наших работ', callback_data="Q02_examples")])
+
+}
 
 client = Client()
 
 
-def translate(text:str, dist_lang: str, source_lang: str = 'English') -> str:
+def translate(text:str, dist_lang: str, source_lang: str = 'Russian') -> str:
     '''
     Translate using openAI from source_lang into dist_lang
     :param text:
@@ -82,27 +113,107 @@ def translate(text:str, dist_lang: str, source_lang: str = 'English') -> str:
     text = f"Translate the following text from {source_lang} into {dist_lang}: {text}."\
            " Give only translation in your reply."
     prompt = [{'role':'user', 'content': text}]
-    response = client.chat.completions.create(model=MODEL, messages=prompt)
-    print(text, response.choices[0].message.content)
-    return response.choices[0].message.content
+    try:
+        response = client.chat.completions.create(model=MODEL, messages=prompt)
+        result = response.choices[0].message.content
+    except:
+        result = text
+    return result
 
 
 @bot.message_handler(commands=['start'])
 def start_bot(message: telebot.types.Message):
     user_id = message.from_user.id
+    print(user_id)
     chat_id = message.chat.id
     user_name = message.from_user.first_name
     if message.from_user.last_name:
         user_name = ' '.join([user_name, message.from_user.last_name])
     if user_id not in users.keys():
         users[user_id] = User(user_id=user_id, chat_id=chat_id, name=user_name, language=LANG)
-    start_message = f'{user_name}, this chatbot powered by openAI may tell about our products in any language of your choice.'\
-                    ' Use commands to change language and to communicate with me.'
-    if users[user_id].language != 'English':
-        start_message = translate(' Use commands to change language and to communicate with me.',
+    start_message = f'{user_name}, добрый день! Я помогу вам во взаимодействии с компанией XPAGE.'
+    if users[user_id].language != 'Russian':
+        start_message = translate(start_message,
                                   users[user_id].language)
     start_message = escape(start_message)
-    bot.send_message(chat_id=chat_id, text=start_message)
+    msg = bot.send_message(chat_id=chat_id, text=start_message)
+    getting_know(msg)
+
+
+def getting_know(message: telebot.types.Message):
+    user_id = message.from_user.id
+    chat_id = message.chat.id
+    text = escape(questions['GettingKnow'].text)
+    kb_getting_know = telebot.types.InlineKeyboardMarkup(row_width=2)
+    kb_getting_know.add(*questions['GettingKnow'].buttons)
+    bot.send_message(chat_id=chat_id, text=text, reply_markup=kb_getting_know)
+
+
+@bot.callback_query_handler(func=lambda query: query.data.startswith("Q01"))
+def getting_know_query(query: telebot.types.CallbackQuery):
+    user_id = query.from_user.id
+    chat_id = query.message.chat.id
+    if query.data[-1:] == "N":
+        new_user(query.message, user_id = user_id)
+    if query.data[-1:] == "Y":
+        ask_contract_number(user_id, chat_id)
+
+
+def ask_contract_number(user_id, chat_id):
+    text = escape(questions['ContractNum'].text)
+    msg = bot.send_message(chat_id=chat_id, text=text)
+    bot.register_next_step_handler(msg, get_contract_number)
+
+
+def get_contract_number(message: telebot.types.Message):
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    contract_number = message.text
+    users[user_id].contract_number = contract_number
+    text = f'Вы ввели номер договора: {contract_number}'
+    text = escape(text)
+    bot.send_message(chat_id=chat_id, text=text)
+    contract_number = contract_number.strip()
+    for contract in contracts:
+        if contract['num'] == contract_number:
+            text = f"Статус работы по вашему договору: {contract['status']}"
+            text = escape(text)
+            bot.send_message(chat_id=chat_id, text=text)
+            break
+    else:
+        text = escape('Проверьте, верно ли вы ввели номер договора. Я не нашёл его в моей базе данных.')
+        bot.send_message(chat_id=chat_id, text=text)
+
+
+def new_user(message: telebot.types.Message, user_id):
+    chat_id = message.chat.id
+    text = escape(questions['NewUser'].text)
+    kb_new_user = telebot.types.InlineKeyboardMarkup(row_width=1)
+    kb_new_user.add(*questions['NewUser'].buttons)
+    msg = bot.send_message(chat_id=chat_id, text=text, reply_markup=kb_new_user)
+
+
+@bot.callback_query_handler(func=lambda query: query.data.startswith("Q02"))
+def info_for_new_user(query: telebot.types.CallbackQuery):
+    user_id = query.from_user.id
+    chat_id = query.message.chat.id
+    result = query.data.split('_')[1]
+    if result == "contract":
+        bot.send_message(chat_id=chat_id,
+                         text = "<a href='https://telegra.ph/SHablon-dogovora-04-06'>Шаблон договора</a>",
+                         parse_mode='html')
+    if result == "about":
+        text = "Наша компания **XPage** специализируется на разработке корпоративных сайтов,"\
+               " мобильных приложений и цифровых услуг. Наша область экспертизы - создание профессиональных"\
+               " продуктов в сфере информационных технологий для бизнеса в области спорта, промышленности и "\
+               " интернет-магазинов."
+        text = escape(text)
+        bot.send_message(chat_id=chat_id, text=text, parse_mode='markdown')
+    if result == "examples":
+        text = "Мы разработали корпоративные сайты для Хоккейного клуба Трактор, спортивного телеканала Старт, "\
+               "федеральной сети ломбардов Фианит-Ломбард, фабрики для производства гофраупаковки и множество других."
+        text = escape(text)
+        bot.send_message(chat_id=chat_id, text=text)
 
 @bot.message_handler(commands=['lang'])
 def select_language(message: telebot.types.Message):
